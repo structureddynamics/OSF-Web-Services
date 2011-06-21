c32962fba87462363ee1ea782729dd795f2203d8
Frederick Giasson <fred@fgiasson.com>
2011-06-21 17:41:27 +0000

    Remove the double encoding control code of the xmlEncode() class because it was preventing data, into the xml file, from being properly encoded.

--- framework/ProcessorXML.php
+++ framework/ProcessorXML.php
@@ -928,7 +928,7 @@ class ProcessorXML
     // Replace all the possible entities by their character. That way, we won't "double encode" 
     // these entities. Otherwise, we can endup with things such as "&amp;amp;" which some
     // XML parsers doesn't seem to like (and throws errors).
-    $string = str_replace(array ("%5C", "&amp;", "&lt;", "&gt;"), array ("\\", "&", "<", ">"), $string);
+    //$string = str_replace(array ("%5C", "&amp;", "&lt;", "&gt;"), array ("\\", "&", "<", ">"), $string);
     
     return str_replace(array ("\\", "&", "<", ">"), array ("%5C", "&amp;", "&lt;", "&gt;"), $string); 
   } 

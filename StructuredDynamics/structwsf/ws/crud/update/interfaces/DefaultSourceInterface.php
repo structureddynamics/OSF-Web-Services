2628b12ce187b602fdb7df9bf15d78a74c553b00
Frederick Giasson <fred@fgiasson.com>
2013-02-11 15:09:07 -0500

    (1) fix an issue with a missing use declaration for CrudRead
    (2) Making the closure of the inferred types

--- StructuredDynamics/structwsf/ws/crud/update/interfaces/DefaultSourceInterface.php
+++ StructuredDynamics/structwsf/ws/crud/update/interfaces/DefaultSourceInterface.php
@@ -10,6 +10,7 @@
   use \StructuredDynamics\structwsf\ws\framework\ClassNode;
   use \StructuredDynamics\structwsf\ws\framework\PropertyHierarchy;
   use \StructuredDynamics\structwsf\ws\framework\propertyNode;
+  use \StructuredDynamics\structwsf\ws\crud\read\CrudRead;  
   
   class DefaultSourceInterface extends SourceInterface
   {
@@ -1147,6 +1148,9 @@
             {
               $superClasses = $classHierarchy->getSuperClasses($type);
 
+              // Add the type to make the closure of the set of inferred types
+              array_push($inferredTypes, $type);              
+              
               foreach($superClasses as $sc)
               {
                 if(array_search($sc->name, $inferredTypes) === FALSE)
